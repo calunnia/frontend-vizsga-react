aa83f931fa35af501fa584999674450c5abab3ad
Erika Szilagyi <calunnia@gmail.com>
2021-04-26 17:35:52 +0200

    No: 4 commit. this is a 1200 seconds auto commit. 26/04/2021 - 17:15:50.76

--- src/components/Pet/Pet.jsx
+++ src/components/Pet/Pet.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 
 function Pet({pet,i}) {
 
     console.log('pet=', pet);
 
-    const [ vaccinated, setVa]
+    const [ vaccinated, setVaccinated] = useState(pet.isVaccinated)
+    const [data, setData] =useState([])
+    const [loading, setLoading] = useState(false)
+    
+    
+    const startPost =(petname,vaccinated) => {
 
+      
+        setLoading(true)
+        setData([])
+        
+        fetch(`/api/pets`,{
+            method:"POST",
+            body: JSON.stringify({name:petname ,isVaccinated:vaccinated })
+            )
+          .then((resopnse) => (resopnse.json()))
+          .then((responseAdat) => { setData(responseAdat);
+                                    setVaccinated(vaccinated);               
+          })
+          .catch(error=>{
+                          console.log('error=',error );
+                          setData(null)
+          })
+          .finally( () => {
+                            console.log('fetch end');
+                            setLoading(false)
+          })
+        
+        }
+    
     return (
         <div className="Pet">
            { pet.name } / {pet.animal}
-           <button onClick={()=>(setVaccinated(!vaccinated))}>Vaccinated</button>
+           <button onClick={()=>(startPost(pet.name, !vaccinated))}>
+            {
+
+                vaccinated
+                            ? 'Vaccinated'
+                            : ' Not Vaccinated'
+            }   
+               
+     
+              
+               </button>
         </div>
     )
 }
