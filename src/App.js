fea38e5101554407b81ab5df893e4b8c40abb2ca
Erika Szilagyi <calunnia@gmail.com>
2021-04-26 16:35:37 +0200

    No: 1 commit. this is a 1200 seconds auto commit. 26/04/2021 - 16:15:36.62

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import './App.css'
+import React, {useState} from 'react'
 
 const App = () => {
 
+const [data, setData] =useState([])
+const [serach,setSearch] = useState("")
+const [loading, setLoading] = useState(false)
+
+
+const startFetch =() => {
+ setLoading(true)
+ setData([])
+ 
+  
+    setLoading(true)
+    setData([])
+    
+    fetch(`/api/clinets?search=${search}`)
+      .then((resopnse) => (resopnse.json()))
+      .then((responseAdat)=>(setData(responseAdat)))
+      .catch(error=>{
+                      console.log('error=',error );
+                      setData(null)
+      })
+      .finally( () => {
+                        console.log('fetch end');
+                        setLoading(false)
+      })
+    
+    
+    
+    console.log('data', data);
+
+
+
+}
+
+
   return (
     <div className="App">
-      Todo...
+      <h1>Clients</h1>
+      <input type="text" onChange={(ev)=>(setSearch(ev.target.value))}/>
+      <button onClick={()=>(startFetch())}>Search</button>
+
+      
     </div>
   )
 }
